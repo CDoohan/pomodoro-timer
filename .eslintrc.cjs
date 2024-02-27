dc9bfd4dd7bda742c82dbe3d855baf3988e4c4d1
Caio Doohan <caiodoohan@Caios-MacBook-Pro-2.local>
2024-02-27 14:47:02 -0300

    Initial configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "@rocketseat/eslint-config/react"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescripit-esling/no-namespace": "off"
   },
 }
+
